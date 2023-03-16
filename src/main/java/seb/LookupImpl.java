53c34e328ecc4d25ecf0a7b52d35ae892cd7cccb
SebMarson <sebmarsontk@gmail.com>
2023-03-16 18:12:58 +0000

    Minor changes to distinguish from other bundles

--- src/main/java/seb/LookupImpl.java
+++ src/main/java/seb/LookupImpl.java
@@ -2,18 +2,19 @@ package seb;
 
 import felixstuff.Lookup;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.monitor.FileEntry;
 
 import java.util.Map;
 
 public class LookupImpl implements Lookup {
 
-    private final Map<String, String> lookupMap = Map.of("key", "value",
+    private final Map<String, String> lookupMap = Map.of("key", "bundleOne",
             "seb", "dev");
 
     @Override
     public Object lookup(String s) {
         try {
-            FileUtils util = new FileUtils();
+            FileEntry fileEntry = null;
         } catch (Exception e) {
             System.out.println("Failed to create a new FileUtils from commons io library");
         }
